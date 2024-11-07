5a7d8ff6287ed84924f9185cd3359ea4e9388b65
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2024-11-07 19:38:25 +0900

    Time: 3 ms (94.75%), Space: 17 MB (14.84%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,51 @@
+class Const:
+    FREE = 0
+    PLANTED = 1
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        plantedCount = 0
+        size = len(flowerbed)
+
+        if n == 0:
+            return True
+
+        if size == 1:
+            return flowerbed[0] == Const.FREE
+
+        # left border
+        if flowerbed[0] == Const.FREE:
+            if size > 1:
+                if flowerbed[1] == Const.FREE:
+                    flowerbed[0] = Const.PLANTED
+                    plantedCount += 1
+            else:
+                flowerbed[0] = Const.PLANTED
+                plantedCount += 1
+
+        # middle
+        for idx in range(1, size-1):
+            if flowerbed[idx] != Const.FREE:
+                continue
+            
+            if flowerbed[idx-1] != Const.FREE:
+                continue
+            
+            if flowerbed[idx+1] != Const.FREE:
+                continue
+            
+            flowerbed[idx] = Const.PLANTED
+            plantedCount += 1
+
+        # right border
+        if flowerbed[size-1] == Const.FREE:
+            if size > 1:
+                if flowerbed[size-2] == Const.FREE:
+                    flowerbed[size-1] = Const.PLANTED
+                    plantedCount += 1
+            else:
+                flowerbed[size-1] = Const.PLANTED
+                plantedCount += 1
+
+        return plantedCount >= n
+        
