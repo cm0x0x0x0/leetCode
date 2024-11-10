9d2d420f75138e434425e366e3c47a0ad37b3b29
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2024-11-10 17:14:23 +0900

    Time: 24 ms (93.78%), Space: 21.4 MB (97.59%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -1,27 +1,16 @@
 class RecentCounter:
 
     def __init__(self):
-        self.requests = []
+        self.requests = collections.deque()
 
     def ping(self, t: int) -> int:
         self.requests.append(t)
-        startTime = t - 3000
-
-        guessIdx = self.binarySearch(startTime)
-
-        for idx in range(guessIdx, len(self.requests)):
-            if self.requests[idx] >= startTime:
-                return len(self.requests) - idx
+        startTime = t-3000
+        while self.requests[0] < startTime:
+            self.requests.popleft()
         
-        return 0
-    
-    def binarySearch(self, t:int) -> int: # return: startIdx
-        size = len(self.requests)
-        halfIdx = size // 2
-        while self.requests[halfIdx] > t and halfIdx > 0:
-            halfIdx = halfIdx // 2
+        return len(self.requests)
         
-        return halfIdx
 
 
 
