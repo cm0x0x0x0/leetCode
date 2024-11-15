429fe833957cc65c628fd420b6856781d7f8ad71
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2024-11-15 21:56:25 +0900

    Time: 7 ms (53.58%), Space: 17.7 MB (60.68%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,18 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def countBit(n):
-            result = 0
-            pivot = 1
-            while pivot <= n:
-                if n & pivot == pivot:
-                    result += 1
-                pivot = pivot << 1
-            
-            return result
+        result = [0] * (n+1)
+        for i in range(1, n+1):
+            if i & 1 == 1:
+                # odd
+                result[i] = result[i-1] + 1
+            else:
+                # even
+                result[i] = result[i>>1]
 
-        
-        result = []
-        for i in range(n+1):
-            result.append(countBit(i))
-        
         return result
