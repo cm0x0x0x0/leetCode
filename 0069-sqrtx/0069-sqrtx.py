fff3ad2b6f6eb55a5273f38ad32a8942b9c1af97
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2025-11-11 23:48:53 +0900

    Time: 3 ms (62.02%), Space: 18 MB (10.24%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,11 +1,26 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        for n in range(1, x+1):
-            if n*n == x:
-                return n
+        if x == 0 or x == 1:
+            return x
+        low = 0
+        high = x
+        mid = int((low+high)/2)
+
+        while low+1<high:
+            powVal = mid*mid
+            if powVal == x:
+                return mid
             
-            k = n+1
-            if n*n < x and k*k >x:
-                return n
-        
+            k = mid+1
+            if powVal < x and k*k > x:
+                return mid
+            
+            if powVal < x:
+                low = mid
+            else:
+                high = mid
+            
+            mid = int((low+high)/2)
+            
+
         return 0
