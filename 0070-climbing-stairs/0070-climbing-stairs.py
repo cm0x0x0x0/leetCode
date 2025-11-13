20c401827d12dac056d6570ce3bb6afeb0208bee
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2025-11-13 23:15:52 +0900

    Time: 0 ms (100%), Space: 17.9 MB (30.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,24 +1,13 @@
-from collections import deque
-
 class Solution:
     def climbStairs(self, n: int) -> int:
-        q = deque()
-        s = []
-        ways = 0
-        q.append(0)
-        s.append(0)
+        dp = [0] * 46
+        dp[1] = 1
+        dp[2] = 2
+        
+        if n < 3:
+            return dp[n]
 
-        while s:
-            # cur = q.popleft()
-            cur = s.pop()
-            if cur == n:
-                ways += 1
-                continue
-
-            for step in (2, 1):
-                k = cur + step
-                if k <= n:
-                    # q.append(k)
-                    s.append(k)
-
-        return ways
+        for i in range(3, n+1):
+            dp[i] = dp[i-2] + dp[i-1]
+        
+        return dp[n]
