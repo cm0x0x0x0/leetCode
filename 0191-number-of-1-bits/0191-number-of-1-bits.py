45eae49bc9bf55a01f3766393cfb2371fea20e3e
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2025-12-12 01:04:53 +0900

    Time: 0 ms (100%), Space: 17.8 MB (30.87%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,17 +1,8 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        def dec2bin(n: int) -> str:
-            result = ""
-            while n > 0:
-                result = str(n%2) + result
-                n //= 2
-
-            return result
-
-        binNum = dec2bin(n)
         count = 0
-        for c in binNum:
-            if c == "1":
+        for i in range(32):
+            if (n >> i) & 1:
                 count += 1
         
         return count
