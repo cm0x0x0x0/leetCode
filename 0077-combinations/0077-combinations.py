fb47a2ea1afd9e8c0aeb36073aacb2961fbe87bb
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2026-01-21 23:25:05 +0900

    Time: 107 ms (82.98%), Space: 61.4 MB (24.75%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,18 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
+        subResult = []
 
-        def dfs(startIdx, depth, res):
+        def dfs(startIdx, depth):
             if depth == k:
-                result.append(res)
+                result.append(subResult[:])
                 return
             
             for i in range(startIdx, n+1):
-                newRes = res[:]
-                newRes.append(i)
-                dfs(i+1, depth+1, newRes)
+                subResult.append(i)
+                dfs(i+1, depth+1)
+                subResult.pop()
         
-
-        dfs(1, 0, [])
+        dfs(1, 0)
 
         return result
