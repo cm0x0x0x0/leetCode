1d80e044151255d66d1bee732140b3200129caa8
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2026-01-27 23:58:57 +0900

    Time: 0 ms (100%), Space: 19.4 MB (35.09%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,26 @@
+class MyStack:
+    
+    def __init__(self):
+        self.l = list()        
+
+    def push(self, x: int) -> None:
+        self.l.append(x)
+
+    def pop(self) -> int:
+        result = self.l[-1]
+        self.l = self.l[:-1]
+        return result
+
+    def top(self) -> int:
+        return self.l[-1]
+
+    def empty(self) -> bool:
+        return len(self.l) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
