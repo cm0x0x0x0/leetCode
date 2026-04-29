9751b5e93bb9e1e114da5b3ad3875086ca16132a
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2026-04-29 22:55:12 +0900

    Time: 43 ms (55.8%), Space: 46 MB (17.99%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,21 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.arr = [-1] * (10**6 + 1)
+
+    def add(self, key: int) -> None:
+        self.arr[key] = key
+        
+    def remove(self, key: int) -> None:
+        self.arr[key] = -1
+
+    def contains(self, key: int) -> bool:
+        return self.arr[key] != -1
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
