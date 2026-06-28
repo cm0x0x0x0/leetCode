d7f543fad708197662a6a333c25900de22cc2377
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2026-06-29 02:15:24 +0900

    Time: 0 ms (100%), Space: 19.1 MB (93.25%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,19 +3,17 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
         n = len(matrix)
-        for layer in range(n//2):
-            i = layer
-            for j in range(i, n-1-i):
-                temp = matrix[i][j] # temp <- A
-                matrix[i][j] = matrix[n-1-j][i] # A <- D
-                matrix[n-1-j][i] = matrix[n-1-i][n-1-j] # D <- C
-                matrix[n-1-i][n-1-j] = matrix[j][n-1-i] # C <- B
-                matrix[j][n-1-i] = temp # B <- temp(A)
+        # transpose
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
 
-
+        # row reverse
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
                 
 
 
